5f7f07322a76aa85bcc18a78c66c8f4e6eddcc77
keyeleo@163.com <keyeleo@163.com>
2016-05-20 23:12:38 +0800

    Xcode projects;

--- keye_test/keye_client/main.cpp
+++ keye_test/keye_client/main.cpp
@@ -1,15 +1,70 @@
 //
 //  main.cpp
-//  keye_client
+//  keye_server
 //
 //  Created by Vic Liu on 3/11/16.
 //  Copyright © 2016 Vic Liu. All rights reserved.
 //
 
 #include <iostream>
+//#include <keye/keye_pch.hpp>
+#include <keye/keye_fwd.h>
+
+using namespace keye;
+using namespace std;
+
+typedef std::allocator<char>			std_ax_t;
+keye::logger log;
+
+class Alloc:public htio_alloc{
+public:
+    //					Alloc():_kax(64<<20,_sax){}
+    virtual void*	allocate(size_t _Count){return _sax.allocate(_Count);}
+    virtual void	deallocate(void* _Ptr, size_t=0){_sax.deallocate((char*)_Ptr,0);}
+private:
+    std_ax_t		_sax;
+    //	keye_ax_t		_kax;
+};
+
+class my_handler:public work_handler{
+public:
+    my_handler(Alloc& ax):_ax(ax){}
+    virtual void	on_open(keye::sh_type& sh){
+        log<<"connected to server"<<endl;
+        string str("good morning");
+        sh.send((void*)str.data(),str.length());
+    }
+    virtual void	on_close(keye::sh_type&){
+        log<<"connect closed"<<endl;
+    }
+    virtual void	on_read(keye::sh_type& sh,void* buf,size_t len){
+        log<<"read "<<(int)len<<endl;
+    }
+private:
+    Alloc&	_ax;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Alloc a;
+    my_handler h(a);
+    service s(h,a);
+    s.connect("127.0.0.1",12345);
+    
+    bool exit=false;
+    while(!exit)
+#ifdef WIN32
+        Sleep(100000);
+#else
+    usleep(100000);
+#endif
+    /*
+     switch(std::getchar()){
+     case 'x':
+     stop();
+     exit=true;
+     break;
+     }
+     */
+    
     return 0;
 }
